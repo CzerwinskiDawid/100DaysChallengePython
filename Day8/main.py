2636ed27068828683f2cbb4c7387192eccda730b
CzerwinskiDawid <czerwinski.dawidd@gmail.com>
2023-07-03 11:43:54 +0200

    Day8

---
+++ Day8/main.py
@@ -0,0 +1,25 @@
+# def greet(name):
+#     print(f"Hello {name}!")
+#     print(f"You know nothing {name}...")
+#     print(f"You're great {name}!")
+#
+#
+# def multiplication(num1, num2):
+#     print(num1 * num2)
+#
+#
+# def dividing(num1, num2):
+#     print(num1 // num2)
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+#
+#
+# greet("John Snow")
+# multiplication(2, 3)
+# dividing(10, 2)
+greet_with(name="John Snow", location="Winterfell")
